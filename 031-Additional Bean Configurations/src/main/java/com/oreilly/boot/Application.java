afb6fbd3d8ea9f71b9ec82f5f876c3e63882c4d4
Ivan Voitovych <noctua.vt@gmail.com>
2019-03-14 12:09:52 +0200

    First commit

---
+++ 031-Additional Bean Configurations/src/main/java/com/oreilly/boot/Application.java
@@ -0,0 +1,33 @@
+package com.oreilly.boot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+//@Configuration
+//@EnableAutoConfiguraion
+//@ComponentScan() @Import(MyConfiguration.class)
+public class Application {
+
+	@Value("${name}")
+	String name;
+	
+	@Autowired
+	String message;
+	
+	@RequestMapping("/")
+	public String home(){
+		return message;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
